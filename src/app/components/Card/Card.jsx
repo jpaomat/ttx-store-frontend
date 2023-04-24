d33e8bf160082213c3d2cca660b55910ccc12e12
Jenny Matamoros <jpaomat30@gmail.com>
2023-04-24 07:59:06 +0200

    [ADD] Service to get product list

--- src/app/components/Card/Card.jsx
+++ src/app/components/Card/Card.jsx
@@ -1,24 +1,36 @@
+import PropTypes from 'prop-types';
 import './Card.scss';
 
-export const Card = () => {
+export const Card = ({ brand, imgUrl, model, price }) => {
 	return (
 		<div className='card'>
-			<img
-				className='card-image'
-				src='https://itx-frontend-test.onrender.com/images/ZmGrkLRPXOTpxsU4jjAcv.jpg'
-			></img>
+			<img className='card-image' src={imgUrl}></img>
 			<div className='card-content'>
 				<div className='item-description__wrapper'>
-					<p className='item-description'>Acer</p>
-					<p className='item-description'>Iconia Talk S</p>
+					<p className='item-brand'>{brand}</p>
+					<p className='item-model'>{model}</p>
 				</div>
-				<p className='item-price'>150 €</p>
+				{price ?
+					<p className='item-price'>{price} €</p> :
+					<p className='item-no-stock'>No disponible</p>
+				}
 			</div>
 			<div className='card-footer'>
 				<div className='card-footer-content'>
-					<button className='card-button'>Agregar al carro</button>
+					<button className='card-button' disabled={!price}>
+						{ price ? 'Agregar al carro' : 'No disponible'}
+					</button>
 				</div>
 			</div>
 		</div>
 	);
+};
+
+Card.defaultProps = {};
+
+Card.propTypes = {
+	brand: PropTypes.string.isRequired,
+	imgUrl: PropTypes.string.isRequired,
+	model: PropTypes.string.isRequired,
+	price: PropTypes.string.isRequired,
 };
--- src/app/components/CardList/CardList.jsx
+++ src/app/components/CardList/CardList.jsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from 'react';
+// import { getProducts } from '../../services/getProducts.service';
 import { Card } from '../Card/Card';
 import './CardList.scss';
 export const CardList = () => {
-	const dataTest = [1, 1, 1, 1, 11, 1, 1, 1, 11, 1];
+
+	const [products, setData] = useState(null);
+
+	useEffect(() => {
+		fetch('https://itx-frontend-test.onrender.com/api/product')
+			.then(response => response.json())
+			.then(json => setData(json))
+			.catch(error => console.error(error));
+	}, []);
+
 	return (
 		<div className='card-container'>
-			{dataTest.map((e, i) => (
-				<Card key={i} />
+			{products && products.map((product, i) => (
+				<Card key={product.id} {...product} />
 			))}
 		</div>
 	);
