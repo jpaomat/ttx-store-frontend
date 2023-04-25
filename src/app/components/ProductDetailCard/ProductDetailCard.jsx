cba7f1da4c081f69d24cf57ca571a73e0941b5c6
Jenny Matamoros <jpaomat30@gmail.com>
2023-04-25 18:12:28 +0200

    [ADD] The multiselect component

--- src/app/components/ProductDetailCard/ProductDetailCard.jsx
+++ src/app/components/ProductDetailCard/ProductDetailCard.jsx
@@ -1,46 +1,32 @@
+import { useParams } from 'react-router-dom';
 import { Button } from '../../shared/ui/components/Button/Button';
 import './ProductDetailCard.scss';
+import { useEffect, useState } from 'react';
+import { BsCart4 } from 'react-icons/bs';
+import { MultiselectContainer } from '../../shared/ui/components/MultiselectContainer/MultiselectContainer';
 
 export const ProductDetailCard = () => {
-	const data = {
-		brand: 'Acer',
-		model: 'Iconia Talk S',
-		price: '784',
-		imgUrl:
-			'https://itx-frontend-test.onrender.com/images/ZmGrkLRPXOTpxsU4jjAcv.jpg',
-		cpu: 'Quad-core 1.3 GHz Cortex-A53',
-		ram: '2 GB RAM',
-		os: 'Android 6.0 (Marshmallow)',
-		displaySize: '720 x 1280 pixels (~210 ppi pixel density)',
-		battery: 'Non-removable Li-Ion 3400 mAh battery (12.92 Wh)',
-		dimentions: '191.7 x 101 x 9.4 mm (7.55 x 3.98 x 0.37 in)',
-		primaryCamera: ['13 MP', 'autofocus'],
-		secondaryCmera: ['2 MP', '720p'],
-        options: {
-            colors: [
-                {
-                    code: 1000,
-                    name: 'Black'
-                }
-            ],
-            storages: [
-                {
-                    code: 2000,
-                    name: '16 GB'
-                },
-                {
-                    code: 2001,
-                    name: '32 GB'
-                }
-            ]
-        },
-		colors: [
-			'Black',
-			'White'
-		],
-	};
+	const { productId } = useParams();
+	const [selectedProductOptions, setSelectedProductOptions] = useState({
+		id: productId,
+	});
+	const [productSelected, setProductSelected] = useState({
+		brand: '',
+		imgUrl: '',
+		model: '',
+		ram: '',
+		price: '',
+		options: {},
+	});
 
-	const dataKeys = [
+	useEffect(() => {
+		fetch(`https://itx-frontend-test.onrender.com/api/product/${productId}`)
+			.then(response => response.json())
+			.then(json => setProductSelected({ ...json, ...{ productSelected } }))
+			.catch(error => console.error(error));
+	}, []);
+
+	const dataToShowDescription = [
 		'brand',
 		'model',
 		'price',
@@ -54,54 +40,108 @@ export const ProductDetailCard = () => {
 		'secondaryCmera',
 	];
 
+	const selectorsProduct = [
+		{
+			selector: 'storages',
+			codeName: 'storageCode',
+			label: 'Almacenamiento',
+		},
+		{
+			selector: 'colors',
+			codeName: 'colorCode',
+			label: 'Colores',
+		},
+	];
+
+	const getOptions = (codeName, options) => {
+		return options.map(({ code, name }) => ({ codeName, code, name }));
+	};
+
+	const onSelect = optionSelected => {
+		const { codeName, code } = optionSelected[0];
+		setSelectedProductOptions({
+			...selectedProductOptions,
+			...{ [`${codeName}`]: code },
+		});
+	};
+
+	const addItemCart = () => {
+		fetch('https://itx-frontend-test.onrender.com/api/cart', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(selectedProductOptions),
+		})
+			.then(response => response.json())
+			.then(json => console.log('Products in cart', json))
+			.catch(error => console.error(error));
+	};
+
+	const { brand, imgUrl, model, ram, price, options } = productSelected;
+
 	return (
-		<div className='detail-card'>
-			<div className='detail-card__item detail-card__item--image'>
-				<img src={data.imgUrl} alt='' />
+		productSelected.brand && (
+			<div className='detail-card'>
+				<div className='detail-card__item detail-card__item--image'>
+					<img src={imgUrl} alt='' />
+				</div>
+				<div className='detail-card__item item-information'>
+					<div className='item-information__header'>
+						<p className='item-information__header--title'>
+							{`${brand}/${model}/${ram}`}
+						</p>
+						{price ? (
+							<div className='item-price'>
+								<span className='item-information__header--price'>{price}</span>
+								<span className='item-information__header--currrency'>€</span>
+							</div>
+						) : (
+							<p className='item__no-stock '>No disponible</p>
+						)}
+					</div>
+
+					<div className='item-information__body'>
+						<p className='item-information__body--title'>
+							Caracteristicas destacadas
+						</p>
+						<ul className='item-information__body--options'>
+							{dataToShowDescription.map(key => (
+								<li key={key}>
+									<span>
+										<b>{key}: </b> {productSelected[key]}
+									</span>
+								</li>
+							))}
+						</ul>
+					</div>
+
+					<div className='item-information__footer'>
+						<div className='item-information__footer--actions'>
+							{selectorsProduct.map(({ selector, label, codeName }) => (
+								<MultiselectContainer
+									key={selector}
+									selector={selector}
+									label={label}
+									displayValue='name'
+									options={getOptions(codeName, options[selector])}
+									onSelect={onSelect}
+								/>
+							))}
+						</div>
+						<Button size='sm' disabledBtn={!price} onClick={addItemCart}>
+							{price ? (
+								<>
+									<span>Agregar al carrito</span>
+									<BsCart4 className='cart-icon' size={14} />
+								</>
+							) : (
+								<span>No disponible</span>
+							)}
+						</Button>
+					</div>
+				</div>
 			</div>
-			<div className='detail-card__item item-information'>
-				<div className='item-information__header'>
-					<p className='item-information__header--title'>
-						{`${data.brand}/${data.model}/${data.ram}`}
-					</p>
-					<p className='item-information__header--price'>
-					</p>
-					{data.price ?
-						<p className='item-information__header--price'>{data.price} €</p> :
-						<p className='item__no-stock '>No disponible</p>
-					}
-				</div>
-
-				<div className='item-information__body'>
-					<p className='item-information__body--title'>Caracteristicas destacadas</p>
-					<ul className='item-information__body--options'>
-						{dataKeys.map(key => (
-							<li key={key}>
-                                <span><b>{key}: </b> {data[key]}</span>
-							</li>
-						))}
-					</ul>
-				</div>
-
-				<div className='item-information__footer'>
-					<div className='item-information__footer--actions'>
-                        {['Almacenamiento', 'Color', 'Cantidad'].map((action, k) => (
-                            <div className='select-actions' key={k}>
-                                <label className='select-actions__label'>{action}</label>
-                                <select className='select-actions__select' id="actions" name="actions">
-                                    {data.options.storages.map((storage, key) =>
-                                        <option key={key} value={storage.name}>{storage.name}</option>
-                                    )}
-                                </select>
-                            </div>
-                        ))}
-                    </div>
-
-					<Button size='sm' disabledBtn={!data.price}>
-						<span>{ data.price ? 'Agregar al carro' : 'No disponible'}</span>
-					</Button>
-				</div>
-			</div>
-		</div>
+		)
 	);
 };
