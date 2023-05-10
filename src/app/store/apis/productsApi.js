a76d2280c784b91d50468818b58410be929e5868
Jenny Matamoros <jpaomat30@gmail.com>
2023-05-10 11:52:13 +0200

    [ADD] Environment json

--- src/app/store/apis/productsApi.js
+++ src/app/store/apis/productsApi.js
@@ -1,17 +1,20 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import envJson from '../../config/ENV.json';
+
+const productClient = envJson.productClient;
 
 export const productsApi = createApi({
     reducerPath: 'products',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://itx-frontend-test.onrender.com/api'
+        baseUrl: productClient.baseUrl
     }),
     keepUnusedDataFor: 3600,
     endpoints: (builder) => ({
         getProducts: builder.query({
-            query: () => '/product'
+            query: () => productClient.productList
         }),
         getProductsById: builder.query({
-            query: (productId) => `/product/${productId}`
+            query: (productId) => `${productClient.productList}/${productId}`
         })
     })
 });
